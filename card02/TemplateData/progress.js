6659bbd2873909ee2936f069729cc954a2433abe
AllanSamurai <allansamurai@gmail.com>
2019-10-11 22:06:04 -0300

    up3

--- card02/TemplateData/progress.js
+++ card02/TemplateData/progress.js
@@ -1,3 +1,5 @@
+var downloadSize = 4;
+var useDownloadSize = false;
 function UnityProgress(gameInstance, progress) {
   if (!gameInstance.Module)
     return;
@@ -9,9 +11,18 @@ function UnityProgress(gameInstance, progress) {
     gameInstance.progress = document.getElementById("progress");
     gameInstance.progress.empty = document.getElementById("empty");
     gameInstance.progress.full = document.getElementById("full");
+    gameInstance.progress.downloadInfo = document.getElementById('downloadInfo');
   }
-  gameInstance.progress.full.style.width = (100 * progress) + "%";
-  gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
+  gameInstance.progress.full.style.width = (100 * progress / .9) + "%";
+  gameInstance.progress.empty.style.width = (100 * (1 - progress / .9)) + "%";
+  if (useDownloadSize)
+    gameInstance.progress.downloadInfo.innerHTML = (progress / .9 * downloadSize).toFixed(3) + "/" + downloadSize + "mb";
+  else {
+    gameInstance.progress.downloadInfo.innerHTML = Math.round( (progress / .9)*100) + "%";
+  }
+  if (progress >= .9) {
+    gameInstance.progress.downloadInfo.innerHTML = "Tudo pronto! Aguarde a inicialização.";
+  }
 
   if (progress == 1)
     gameInstance.loadScreen.style.display = gameInstance.progress.style.display = "none";
